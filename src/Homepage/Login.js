997fb865301374cad851eb2ac348df89d400e36e
Lionel Beato <lionel.beato@gmail.com>
2021-07-29 23:32:47 -0400

    push google

--- src/Homepage/Login.js
+++ src/Homepage/Login.js
@@ -11,10 +11,17 @@ const Login = () => {
   }
 
   const login = () => {
-    fetch("https://erin-spring-backend.herokuapp.com/users/oidc-principal")
+    fetch("https://erin-spring-backend.herokuapp.com/users/oidc-principal",
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          
+        }
+      })
       .then(response => console.log(response))
+      .then(json => console.log(json))
   }
-
   return (
     <div id="loginform">
       <video autoPlay loop muted={true} height="150px" width="300px">
@@ -23,22 +30,22 @@ const Login = () => {
       <div className="imageLogo">
         <img src={MyflixLogo4} alt="Logo" />
       </div>
+      <GoogleLogin
+        clientId="768435042402-lc8p0l5blqou7u5tjno1m2rf17eo6qv7.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
       <a
         className="button-link"
         href="https://erin-spring-backend.herokuapp.com/oauth2/authorization/google"
       >
-        {/* <GoogleLogin
-          clientId="768435042402-lc8p0l5blqou7u5tjno1m2rf17eo6qv7.apps.googleusercontent.com"
-          buttonText="Login"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        /> */}
         <div id="buttonSign" className="rowLogin">
           <button>Sign in with Google</button>
         </div>
-        <button onClick={login}>click me :)</button>
       </a>
+      <button onClick={login}>click me :)</button>
     </div>
   );
 };
