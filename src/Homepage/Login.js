9b654263f8cc0a03001b39c0566c88b684a872ac
Lionel Beato <lionel.beato@gmail.com>
2021-07-30 01:23:03 -0400

    further refinement

--- src/Homepage/Login.js
+++ src/Homepage/Login.js
@@ -16,10 +16,6 @@ const Login = () => {
       {
         method: "GET",
         credentials: "include",
-        Authorization: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNkZjBhODMxZTA5M2ZhZTFlMjRkNzdkNDc4MzQ0MDVmOTVkMTdiNTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNzY4NDM1MDQyNDAyLWxjOHAwbDVibHFvdTd1NXRqbm8xbTJyZjE3ZW82cXY3LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNzY4NDM1MDQyNDAyLWxjOHAwbDVibHFvdTd1NXRqbm8xbTJyZjE3ZW82cXY3LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAzNDc4MjY0MjUyNzczMzY0NTE4IiwiZW1haWwiOiJsaW9uZWwuYmVhdG9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJhUXVRYy1hSXZpTEI5UGFTNktjaFpRIiwibmFtZSI6Ikxpb25lbCBCZWF0byIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQVRYQUp6SEtDd3d4ZjVZM3E0Nk9YT0huZ2NmLVFZZi1qcVZxRDkzMUtmQz1zOTYtYyIsImdpdmVuX25hbWUiOiJMaW9uZWwiLCJmYW1pbHlfbmFtZSI6IkJlYXRvIiwibG9jYWxlIjoiZW4iLCJpYXQiOjE2Mjc2MjA2MDQsImV4cCI6MTYyNzYyNDIwNCwianRpIjoiYzViZDIzYWQxYzk1NDAzMzc1ZTI0OGEyNzJmZDRlNGU5ZWI2NjA0ZCJ9.Nk63ifPF-WpksJSOnQpyVSdmWdqC2PbOfzwCAZJdqlxuXHDaU1wtprytPjOosWrrAS1y-f4ajopuBnC2TENOtikVMZUiExte8vMdZy-UTvRe6p2VuPy2XgyVv0ompenKh3moXlYdZ8ha3kEqYCkyojctqqkh9fwaWlB-qaNGc-owq4jXe5ihaD8u9tNOl7TJ28qQfiqHjPHL4sRKRhm3bkqUybXjGr49lZS18f_NprxgzeuXpiyfeCq1sWJTOwy6MI0Tj9BNZsEpSWcJSAZmazNZgdRF6JrbUQjsseArSLZstss2QufsSQJ6Uv9hHsvJ3VTkGgOYSHB2bWLkEXUjBA",
-        headers: {
-
-        }
       })
       .then(response => console.log(response))
     // .then(json => console.log(json))
